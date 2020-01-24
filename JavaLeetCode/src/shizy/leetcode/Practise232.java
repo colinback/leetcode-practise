8ff5d64d6d10540bc47019fab6c5b5cff8141c0a
shizy <shizy@cn.ibm.com>
2020-01-24 15:34:36 +0800

    review heap & stack problems

--- JavaLeetCode/src/shizy/leetcode/Practise232.java
+++ JavaLeetCode/src/shizy/leetcode/Practise232.java
@@ -4,69 +4,71 @@ import java.util.Stack;
 
 /*
  * Implement the following operations of a queue using stacks.
- * 
+ *
  * push(x) -- Push element x to the back of queue.
  * pop() -- Removes the element from in front of queue.
  * peek() -- Get the front element.
  * empty() -- Return whether the queue is empty.
- * 
+ *
  * Notes:
- * - You must use only standard operations of a stack -- which means only push to top, 
- *   peek/pop from top, size, and is empty operations are valid.
- * - Depending on your language, stack may not be supported natively. 
- *   You may simulate a stack by using a list or deque (double-ended queue), as long as 
- *   you use only standard operations of a stack.
- * - You may assume that all operations are valid (for example, no pop or peek operations 
- *   will be called on an empty queue).
+ * - You must use only standard operations of a stack -- which means only push
+ * to top, peek/pop from top, size, and is empty operations are valid.
+ * - Depending on your language, stack may not be supported natively.
+ *   You may simulate a stack by using a list or deque (double-ended queue), as
+ * long as you use only standard operations of a stack.
+ * - You may assume that all operations are valid (for example, no pop or peek
+ * operations will be called on an empty queue).
  */
 
 public class Practise232 {
-	class MyQueue {
-		private Stack<Integer> st = null;
-		
-	    /** Initialize your data structure here. */
-	    public MyQueue() {
-	        st = new Stack<Integer>();
-	    }
-	    
-	    /** Push element x to the back of queue. */
-	    public void push(int x) {
-	        Stack<Integer> ns = new Stack<Integer>();
-	        ns.push(x);
-	        ns.addAll(st);
-	        
-	        st = ns; 
-	    }
-	    
-	    /** Removes the element from in front of queue and returns that element. */
-	    public int pop() {
-	        return st.pop();
-	    }
-	    
-	    /** Get the front element. */
-	    public int peek() {
-	        return st.peek();
-	    }
-	    
-	    /** Returns whether the queue is empty. */
-	    public boolean empty() {
-	        return st.isEmpty();
-	    }
-	}
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Practise232 p = new Practise232();
-		
-		MyQueue obj = p.new MyQueue();
-		obj.push(1);
-		obj.push(2);
-		int param_2 = obj.pop();
-		System.out.println(param_2);
-		int param_3 = obj.peek();
-		System.out.println(param_3);
-		boolean param_4 = obj.empty();
-		System.out.println(param_4);
-	}
+  class MyQueue {
+    private Stack<Integer> s1 = null;
+    private Stack<Integer> s2 = null;
 
+    /** Initialize your data structure here. */
+    public MyQueue() {
+      s1 = new Stack<Integer>();
+      s2 = new Stack<Integer>();
+    }
+
+    /** Push element x to the back of queue. */
+    public void push(int x) { s1.add(x); }
+
+    /** Removes the element from in front of queue and returns that element. */
+    public int pop() {
+      if (s2.isEmpty()) {
+        while (!s1.isEmpty()) {
+          s2.push(s1.pop());
+        }
+      }
+      return s2.pop();
+    }
+
+    /** Get the front element. */
+    public int peek() {
+      if (s2.isEmpty()) {
+        while (!s1.isEmpty()) {
+          s2.push(s1.pop());
+        }
+      }
+      return s2.peek();
+    }
+
+    /** Returns whether the queue is empty. */
+    public boolean empty() { return s1.isEmpty() && s2.isEmpty(); }
+  }
+
+  public static void main(String[] args) {
+    Practise232 p = new Practise232();
+
+    MyQueue obj = p.new MyQueue();
+    obj.push(1);
+    obj.push(2);
+    int param_2 = obj.pop();
+    System.out.println(param_2);
+    int param_3 = obj.peek();
+    System.out.println(param_3);
+    boolean param_4 = obj.empty();
+    System.out.println(param_4);
+  }
 }
--- JavaLeetCode/src/shizy/leetcode/Practise295.java
+++ JavaLeetCode/src/shizy/leetcode/Practise295.java
@@ -4,90 +4,89 @@ import java.util.Collections;
 import java.util.PriorityQueue;
 
 /*
- * Median is the middle value in an ordered integer list. 
- * If the size of the list is even, there is no middle value. 
+ * Median is the middle value in an ordered integer list.
+ * If the size of the list is even, there is no middle value.
  * So the median is the mean of the two middle value.
- * 
- * Examples: 
+ *
+ * Examples:
  * [2,3,4] , the median is 3
  * [2,3], the median is (2 + 3) / 2 = 2.5
- * 
+ *
  * Design a data structure that supports the following two operations:
- * 
- * void addNum(int num) - Add a integer number from the data stream to the data structure.
- * double findMedian() - Return the median of all elements so far.
+ *
+ * void addNum(int num) - Add a integer number from the data stream to the data
+ * structure. double findMedian() - Return the median of all elements so far.
  */
 
 public class Practise295 {
-	class MedianFinder {
-		private PriorityQueue<Integer> minHeap = null;
-		private PriorityQueue<Integer> maxHeap = null;
-		
-	    /** initialize your data structure here. */
-	    public MedianFinder() {
-	    		minHeap = new PriorityQueue<>(Collections.reverseOrder());
-	    		maxHeap = new PriorityQueue<>();
-	    }
-	    
-	    public void addNum(int num) {
-	    		if (minHeap.size() == 0) {
-	    			minHeap.offer(num);
-	    			return;
-	    		}
-	    		
-	        if (minHeap.size() == maxHeap.size()) {
-	        		if (num < maxHeap.peek())
-	        			minHeap.offer(num);
-	        		else
-	        			maxHeap.offer(num);
-	        } else if (minHeap.size() > maxHeap.size()) {
-	        		if (minHeap.peek() < num)
-	        			maxHeap.offer(num);
-	        		else {
-	        			int m = minHeap.poll();
-	        			minHeap.offer(num);
-	        			maxHeap.offer(m);
-	        		}
-	        } else {
-	        		if (maxHeap.peek() > num)
-	        			minHeap.offer(num);
-	        		else {
-	        			int m = maxHeap.poll();
-	        			minHeap.offer(m);
-	        			maxHeap.offer(num);
-	        		}
-	        }
-	    }
-	    
-	    public double findMedian() {
-		    	if (minHeap.size() == maxHeap.size())
-		    		return (minHeap.peek() + maxHeap.peek())/2.0;
-		    	else if (minHeap.size() > maxHeap.size())
-		    		return (double)minHeap.peek();
-		    	else 
-		    		return (double)maxHeap.peek();
-	    }
-	}
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Practise295 p = new Practise295();
-		
-		MedianFinder obj = p.new MedianFinder();
-		obj.addNum(4);
-		obj.addNum(5);
-		System.out.println("----");
-		double param_2 = obj.findMedian();
-		System.out.println(param_2);
-		
-		obj.addNum(9);
-		System.out.println("----");
-		double param_3 = obj.findMedian();
-		System.out.println(param_3);
-		
-		obj.addNum(1);
-		System.out.println("----");
-		double param_4 = obj.findMedian();
-		System.out.println(param_4);
-	}
+  class MedianFinder {
+    private PriorityQueue<Integer> minHeap = null;
+    private PriorityQueue<Integer> maxHeap = null;
+
+    /** initialize your data structure here. */
+    public MedianFinder() {
+      minHeap = new PriorityQueue<>(Collections.reverseOrder());
+      maxHeap = new PriorityQueue<>();
+    }
+
+    public void addNum(int num) {
+      if (minHeap.size() == 0) {
+        minHeap.offer(num);
+        return;
+      }
+
+      if (minHeap.size() == maxHeap.size()) {
+        if (num < maxHeap.peek())
+          minHeap.offer(num);
+        else
+          maxHeap.offer(num);
+      } else if (minHeap.size() > maxHeap.size()) {
+        if (minHeap.peek() < num)
+          maxHeap.offer(num);
+        else {
+          int m = minHeap.poll();
+          minHeap.offer(num);
+          maxHeap.offer(m);
+        }
+      } else {
+        if (maxHeap.peek() > num)
+          minHeap.offer(num);
+        else {
+          int m = maxHeap.poll();
+          minHeap.offer(m);
+          maxHeap.offer(num);
+        }
+      }
+    }
+
+    public double findMedian() {
+      if (minHeap.size() == maxHeap.size())
+        return (minHeap.peek() + maxHeap.peek()) / 2.0;
+      else if (minHeap.size() > maxHeap.size())
+        return (double)minHeap.peek();
+      else
+        return (double)maxHeap.peek();
+    }
+  }
+
+  public static void main(String[] args) {
+    Practise295 p = new Practise295();
+
+    MedianFinder obj = p.new MedianFinder();
+    obj.addNum(4);
+    obj.addNum(5);
+    System.out.println("----");
+    double param_2 = obj.findMedian();
+    System.out.println(param_2);
+
+    obj.addNum(9);
+    System.out.println("----");
+    double param_3 = obj.findMedian();
+    System.out.println(param_3);
+
+    obj.addNum(1);
+    System.out.println("----");
+    double param_4 = obj.findMedian();
+    System.out.println(param_4);
+  }
 }
